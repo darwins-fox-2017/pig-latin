685cb23f1c215e0020673c5983e3b77db3d5ea92
Samuel PH Sinaga <samuelphsinaga@gmail.com>
2017-01-31 14:06:16 +0700

    pig latin done

--- pig_latin.js
+++ pig_latin.js
@@ -19,19 +19,31 @@ rl.on('line', (line) => {
 });
 
 
-function pig_latin(input){
+function kata(input){
   input = input.toLowerCase().split("")
-  var result = '';
+  var temp1 = [];
+  var temp2 = [];
 
   if (input[0] === 'a' || input[0] === 'i' || input[0] === 'u' || input[0] === 'e' || input[0] === 'o'){
     var finalresult2 = input.join('')
-    return finalresult2
+    temp1.push(finalresult2)
+    return temp1.toString()
   }
   if (input[0] !== 'a' || input[0] !== 'i' || input[0] !== 'u' || input[0] !== 'e' || input[0] !== 'o'){
     //return "not vowel"
     let temp = input[0]
     input.splice(0,1);
-    var finalresult1 = input.join('')+temp+'ay';
-    return finalresult1
+    var finalresult1 = input.join('')+temp+'ay.';
+    temp2.push(finalresult1)
+    return temp2.toString()
   }
 }
+
+function pig_latin(kalimat){
+  kalimat = kalimat.split(" ")
+  var res=[];
+  for(var i=0; i<kalimat.length;i++){
+    res.push(kata(kalimat[i]));
+  }
+  return res.join(" ");
+  }
