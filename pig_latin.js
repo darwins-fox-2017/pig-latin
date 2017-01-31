a13e1131eabb63346aeb1c5f54bfeb2018196574
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-01-31 14:36:48 +0700

    Danang pig latin

--- pig_latin.js
+++ pig_latin.js
@@ -2,3 +2,40 @@
 
 //use readline to fix this challenge
 const readline = require('readline');
+
+let convertPigLatin = (word) => {
+    let vowel = ['A','I','U','E','O','a','i','u','e','o'];
+    let splitWord = word.split('');
+    for(let i=0;i<splitWord.length;i++) {
+        if(i === 0 && vowel.indexOf(splitWord[i]) > -1) {
+            return word;
+        } else if (vowel.indexOf(splitWord[i]) > -1) {
+            let concatConsonant = word.substring(0, i); //t hank =>   
+            splitWord.splice(0, concatConsonant.length);
+            splitWord.splice(word.length-1, 0, concatConsonant); 
+            return splitWord.join('')+'ay';
+        }
+    }
+}
+
+let convertSentencePigLatin = (sentence) => {
+    let splitSpaceInput = sentence.split(' ');
+    let result = '';
+    let wordConvertedCount = 0;
+    for(let i=0;i<splitSpaceInput.length;i++) {
+       result += (convertPigLatin(splitSpaceInput[i])+' '); 
+    }
+    return result;
+}
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: 'Hallo User, kata apa yang akan anda jadikan pig latin hari ini? '
+});
+
+rl.prompt();
+
+rl.on('line', (input) => {
+    console.log(convertSentencePigLatin(input));   
+});
